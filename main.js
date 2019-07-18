6eb57d260184522b9c8697ce5f16c72db13d97d9
Dredge <26813102+RamirezJA@users.noreply.github.com>
2019-07-18 16:48:33 -0700

    Cap-2-Complete

---
+++ main.js
@@ -0,0 +1,30 @@
+const express = require("express"),
+    app = express(),
+    homeController = require("./controllers/homeController"),
+    layouts = require("express-ejs-layouts");
+
+    app.set("view engine", "ejs");
+    app.set("port", process.env.PORT || 3000);
+
+    app.use(
+        express.urlencoded({
+            extended: false
+        })
+    );
+    app.use(express.json());
+    app.use(layouts);
+    app.use(express.static("public"));
+
+    app.get("/", (req, res) => {
+        res.render("index");
+    });
+
+    app.get("/courses", homeController.showCourses);
+    app.get("/contact", homeController.showSignUp);
+    app.get("/contact", homeController.postedSignUpForm);
+
+    app.listen(app.get("port"), () => {
+        console.log(
+            `Server running at htpp://localhost:${app.get("port")}`
+        );
+    });

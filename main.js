ab9ed080ab98f8118f66552ba2ec5f810e82db68
Dredge <26813102+RamirezJA@users.noreply.github.com>
2019-07-24 22:48:05 -0700

    Subscriber page w/ post

--- main.js
+++ main.js
@@ -3,7 +3,11 @@ const express = require("express"),
     homeController = require("./controllers/homeController"),
     errorController = require("./controllers/errorController"),
     layouts = require("express-ejs-layouts"),
-    mongoose = require("mongoose");
+    mongoose = require("mongoose"),
+    subscribersController = require("./controllers/subscribersController"),
+    Subscriber = require("./models/subscriber");
+
+    mongoose.Promise = global.Promise;
 
     mongoose.connect(
         "mongodb://localhost:27017/recipe_db",
@@ -14,34 +18,10 @@ const express = require("express"),
         console.log("Successfully connected to MongoDB using Mongoose!")
     })
 
-    const subscriberSchema = mongoose.Schema({
-        name:String,
-        email:String,
-        zipCode:Number
-    });
-
-    const Subscriber = mongoose.model("Subscriber", subscriberSchema)
-
-    var subscriber1 = new Subscriber({
-        name:"Jon Wexler",
-        email:"jon@jonwexler.com"
-    });
-
-    subscriber1.save((error, savedDocument) => {
-        if (error) console.log(error);
-        console.log(savedDocument);
-    });
-
-    Subscriber.create(
-        {
-            name:"Jon Wexler",
-            email:"jon@jonwexler.com"   
-        },
-        function (error, savedDocument) {
-            if (error) console.log(error);
-            console.log(savedDocument);
-        }
-    );
+    var myQuery = Subscriber.findOne({ name: "Jon Wexler"}).where("email", /wexler/);
+    myQuery.exec((error, data) => {
+        if(data) console.log(data.name)
+    })
 
     app.set("view engine", "ejs");
     app.set("port", process.env.PORT || 3000);
@@ -58,11 +38,19 @@ const express = require("express"),
     app.get("/", (req, res) => {
         res.render("index");
     });
-
+    app.get("/contact", subscribersController.getSubscriptionPage);
     app.get("/courses", homeController.showCourses);
     app.get("/contact", homeController.showSignUp);
     app.get("/contact", homeController.postedSignUpForm);
+    app.get("/subscribers", subscribersController.getAllSubscribers,
+    (req,res, next) => {
+        
+        res.render("subscribers", {subscribers: req.data});
+    });
 
+    app.post("/subscribe", subscribersController.saveSubscriber);
+    
+    //At end
     app.use(errorController.pageNotFoundError);
     app.use(errorController.internalServerError);
 

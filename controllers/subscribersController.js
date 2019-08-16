3000490f0dcbc2875508f672817d922efe8589b3
Dredge <26813102+RamirezJA@users.noreply.github.com>
2019-08-16 16:57:09 -0700

    sub newsletter form + view subs pg

--- controllers/subscribersController.js
+++ controllers/subscribersController.js
@@ -1,26 +1,38 @@
 const Subscriber = require("../models/subscriber");
 
-    exports.getAllSubscribers = ((req, res, next) => {
-        Subscriber.find({}, (error, subscribers) => {
-            if (error) next(error);
-            req.data = subscribers;
-            next();
+exports.getAllSubscribers = (req, res) => {
+    Subscriber.find({})
+        .exec()
+        .then((subscribers) => {
+            res.render("subscribers", {
+                subscribers: subscribers
+            });
+        })
+        .catch((error) => {
+            console.log(error.message);
+            return [];
+        })
+        .then(() => {
+            console.log("promise complete");
         });
-    });
+    };
 
     exports.getSubscriptionPage = (req, res) => {
         res.render("contact");
     };
 
     exports.saveSubscriber = (req, res) => {
-        let newSubscriber = new Subscriber({
+        let newSubscriber = new Subscriber( {
             name: req.body.name,
             email: req.body.email,
             zipCode: req.body.zipCode
         });
 
-        newSubscriber.save((error, result) => {
-            if(error) res.send(error);
-            res.render("thanks");
-        });
+        newSubscriber.save()
+            .then( () => {
+                res.render("thanks");
+            })
+            .catch(error => {
+                res.send(error);
+            });
     };
--- main.js
+++ main.js
@@ -1,61 +1,43 @@
+"use strict";
+
 const express = require("express"),
-    app = express(),
-    homeController = require("./controllers/homeController"),
-    errorController = require("./controllers/errorController"),
-    layouts = require("express-ejs-layouts"),
-    mongoose = require("mongoose"),
-    subscribersController = require("./controllers/subscribersController"),
-    Subscriber = require("./models/subscriber");
+  app = express(),
+  homeController = require("./controllers/homeController"),
+  errorController = require("./controllers/errorController"),
+  layouts = require("express-ejs-layouts"),
+  subscriberController = require("./controllers/subscribersController"),
+  mongoose = require("mongoose");
 
-    mongoose.Promise = global.Promise;
+  mongoose.Promise = global.Promise;
+  mongoose.connect(
+    "mongodb://localhost:27017/confetti_cuisine",
+    {useNewUrlParser: true}
+  );
 
-    mongoose.connect(
-        "mongodb://localhost:27017/recipe_db",
-        {useNewUrlParser: true}
-    );
-    const db = mongoose.connection;
-    db.once("open", () => {
-        console.log("Successfully connected to MongoDB using Mongoose!")
-    })
+app.set("view engine", "ejs");
+app.set("port", process.env.PORT || 3000);
+app.use(
+  express.urlencoded({
+    extended: false
+  })
+);
+app.use(express.json());
+app.use(layouts);
+app.use(express.static("public"));
 
-    var myQuery = Subscriber.findOne({ name: "Jon Wexler"}).where("email", /wexler/);
-    myQuery.exec((error, data) => {
-        if(data) console.log(data.name)
-    })
+app.get("/", (req, res) => {
+  res.render("index");
+});
 
-    app.set("view engine", "ejs");
-    app.set("port", process.env.PORT || 3000);
+app.get("/courses", homeController.showCourses);
+app.get("/subscribers", subscriberController.getAllSubscribers);
+app.get("/contact", subscriberController.getSubscriptionPage);
+app.post("/subscribe", subscriberController.saveSubscriber);
+app.post("/contact", homeController.postedSignUpForm);
 
-    app.use(
-        express.urlencoded({
-            extended: false
-        })
-    );
-    app.use(express.json());
-    app.use(layouts);
-    app.use(express.static("public"));
+app.use(errorController.pageNotFoundError);
+app.use(errorController.internalServerError);
 
-    app.get("/", (req, res) => {
-        res.render("index");
-    });
-    app.get("/contact", subscribersController.getSubscriptionPage);
-    app.get("/courses", homeController.showCourses);
-    app.get("/contact", homeController.showSignUp);
-    app.get("/contact", homeController.postedSignUpForm);
-    app.get("/subscribers", subscribersController.getAllSubscribers,
-    (req,res, next) => {
-        
-        res.render("subscribers", {subscribers: req.data});
-    });
-
-    app.post("/subscribe", subscribersController.saveSubscriber);
-    
-    //At end
-    app.use(errorController.pageNotFoundError);
-    app.use(errorController.internalServerError);
-
-    app.listen(app.get("port"), () => {
-        console.log(
-            `Server running at htpp://localhost:${app.get("port")}`
-        );
-    });
+app.listen(app.get("port"), () => {
+  console.log(`Server running at http://localhost:${app.get("port")}`);
+});

7e69f3e86421cb7ba25c84a0be83e4341389b097
Dredge <26813102+RamirezJA@users.noreply.github.com>
2019-09-09 18:31:22 -0700

    Repl.js fix

--- repl.js
+++ repl.js
@@ -1,25 +1,60 @@
 const mongoose = require("mongoose"),
-  User = require("./models/user")
-
-
+  Subscriber = require("./models/subscriber"),
+  Course = require("./models/course");
+var testCourse, testSubscriber;
 mongoose.connect(
   "mongodb://localhost:27017/recipe_db",
-  {useNewUrlParser: true}
+  { useNewUrlParser: true }
 );
-
 mongoose.Promise = global.Promise;
-
-var testUser;
-User.create({
-  name: {
-    first: "Jon",
-    last: "Wexler"
-  },
-  email: "jon@jonwexler.com",
-  password: "pass123"
-})
-  .then(user => testUser = user)
-  .catch(error => console.log(error.message));
-
-
-
+Subscriber.remove({})
+  .then(items => console.log(`Removed ${items.n} records!`))
+  .then(() => {
+    return Course.remove({});
+  })
+  .then(items => console.log(`Removed ${items.n} records!`))
+  .then(() => {
+    return Subscriber.create({
+      name: "Jon",
+      email: "jon@jonwexler.com",
+      zipCode: "12345"
+    });
+  })
+  .then(subscriber => {
+    console.log(`Created Subscriber: ${subscriber.getInfo()}`);
+  })
+  .then(() => {
+    return Subscriber.findOne({
+      name: "Jon"
+    });
+  })
+  .then(subscriber => {
+    testSubscriber = subscriber;
+    console.log(`Found one subscriber: ${subscriber.getInfo()}`);
+  })
+  .then(() => {
+    return Course.create({
+      title: "Tomato Land",
+      description: "Locally farmed tomatoes only",
+      zipCode: 12345,
+      items: ["cherry", "heirloom"]
+    });
+  })
+  .then(course => {
+    testCourse = course;
+    console.log(`Created course: ${course.title}`);
+  })
+  .then(() => {
+    testSubscriber.courses.push(testCourse);
+    testSubscriber.save();
+  })
+  .then(() => {
+    return Subscriber.populate(testSubscriber, "courses");
+  })
+  .then(subscriber => console.log(subscriber))
+  .then(() => {
+    return Subscriber.find({
+      courses: mongoose.Types.ObjectId(testCourse._id)
+    });
+  })
+  .then(subscriber => console.log(subscriber));

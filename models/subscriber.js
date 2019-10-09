5c09c24036f23a9e900e459670e7d2a936ad14df
Dredge <26813102+RamirezJA@users.noreply.github.com>
2019-10-08 23:56:13 -0700

    + user/sub model data share

--- models/subscriber.js
+++ models/subscriber.js
@@ -1,6 +1,8 @@
 const mongoose = require("mongoose"),
+    {Schema} = mongoose,
+    User = require("./user"),
 
-    subscriberSchema = new mongoose.Schema({
+    subscriberSchema = new Schema({
         name:{
             type: String,
             required: true
@@ -16,8 +18,13 @@ const mongoose = require("mongoose"),
             min: [10000, "zip code too short"],
             max: 99999
         },
-        courses: [{ type: mongoose.Schema.Types.ObjectId, ref: "Course"}]
-    }, {
+        courses: [{ type: mongoose.Schema.Types.ObjectId, ref: "Course"}],
+        userAccount: {
+            type: Schema.Types.ObjectId,
+            ref: "User"
+        }
+    },   
+    {
         timestamps: true
     });
 
@@ -30,5 +37,24 @@ const mongoose = require("mongoose"),
             .find({zipCode: this.zipCode})
             .exec();
     };
+
+    subscriberSchema.pre("save", function(next) {
+        let subscriber = this;
+        if (subscriber.userAccount === undefined) {
+            User.findOne({
+                email: subscriber.email
+            })
+                .then(user => {
+                    subscriber.userAccount = user;
+                    next();
+                })
+                .catch(error => {
+                    console.log(`Error in connecting subscriber:${error.message}`);
+                    next(error);
+                });
+        }else {
+            next();
+        }
+    });
 
     module.exports = mongoose.model("Subscriber", subscriberSchema);

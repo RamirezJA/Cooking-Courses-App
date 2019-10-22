2470e93e3d5f870a9d95a4dd0bfb5a3284701066
Dredge <26813102+RamirezJA@users.noreply.github.com>
2019-10-22 15:43:31 -0700

    bcrypt to user via pre

--- models/user.js
+++ models/user.js
@@ -1,6 +1,7 @@
 "use strict";
 
 const mongoose = require("mongoose"),
+  bcrypt = require("bcrypt"),
   { Schema } = mongoose,
   Subscriber = require("./subscriber"),
   userSchema = new Schema(
@@ -63,5 +64,24 @@ userSchema.pre("save", function(next) {
     next();
   }
 });
+
+userSchema.pre("save", function(next) {
+  let user = this;
+  bcrypt
+    .hash(user.password, 10)
+    .then(hash => {
+      user.password = hash;
+      next();
+    })
+    .catch(error => {
+      console.log(`Error in hashing password: ${error.message}`);
+      next(error);
+    });
+});
+
+userSchema.methods.passwordComparison = function(inputPassword) {
+  let user = this;
+  return bcrypt.compare(inputPassword, user.password);
+};
 
 module.exports = mongoose.model("User", userSchema);

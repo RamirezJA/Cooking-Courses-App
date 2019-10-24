4addfd66d6b4c2469d9696cd224e40a42411eef4
Dredge <26813102+RamirezJA@users.noreply.github.com>
2019-10-24 00:29:43 -0700

    router logout +

--- main.js
+++ main.js
@@ -92,6 +92,7 @@ router.post(
 router.get("/users/login", usersController.login);
 router.get("/users/login", usersController.login);
 router.post("/users/login", usersController.authenticate);
+router.get("/users/logout", usersController.logout, usersController.redirectView);
 router.get("/users/:id/edit", usersController.edit);
 router.put("/users/:id/update", usersController.update, usersController.redirectView);
 router.delete("/users/:id/delete", usersController.delete, usersController.redirectView);
